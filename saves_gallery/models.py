6e11efbd55b4ecd61648fb8042eb8249a6161468
PritenMakwana44 <priten.makwana7@gmail.com>
2023-03-10 12:50:48 +0000

    Fully working gallery,saves,comments

--- saves_gallery/models.py
+++ saves_gallery/models.py
@@ -6,7 +6,7 @@ from gallery.models import Gallery
 class Save_gallery(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     gallery = models.ForeignKey(
-        Gallery, related_name='saved', on_delete=models.CASCADE)
+        Gallery, related_name='saved_gallery', on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now_add=True)
 
     class Meta:
