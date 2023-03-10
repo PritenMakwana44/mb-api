8d3cccb801fe36a7f4269996fc0bea0655332d8c
PritenMakwana44 <priten.makwana7@gmail.com>
2023-03-10 20:09:06 +0000

    Contact

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    firstname = models.CharField(max_length=55)
+    lastname = models.CharField(max_length=55)
+    email = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f'{self.firstname} {self.lastname}'
