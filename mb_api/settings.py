3a6cbb1bc25ba29d7cd3090111a3312116d7842a
PritenMakwana44 <priten.makwana7@gmail.com>
2023-03-20 22:38:14 +0000

    fix settings

--- mb_api/settings.py
+++ mb_api/settings.py
@@ -59,7 +59,7 @@ REST_AUTH_SERIALIZERS = {
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
@@ -117,11 +117,26 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ALLOWED_HOSTS = [ os.environ.get('ALLOWED_HOST'), 'localhost',] 
+ALLOWED_HOSTS = [
+    os.environ.get('ALLOWED_HOST'),
+    'localhost',
+]
 
-if 'CLIENT_ORIGIN' in os.environ: CORS_ALLOWED_ORIGINS = [ os.environ.get('CLIENT_ORIGIN') ]
+if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGINS = [
+        os.environ.get('CLIENT_ORIGIN')
+    ]
 
-if 'CLIENT_ORIGIN_DEV' in os.environ: extracted_url = re.match( r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE ).group(0) CORS_ALLOWED_ORIGIN_REGEXES = [ rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$", ]
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(
+        r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE
+    ).group(0)
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
+    ]
+
+
+
 
 CORS_ALLOW_CREDENTIALS = True
 
