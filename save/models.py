606df3f9a5791f50a58cf97aed692bc73b48a7b3
PritenMakwana44 <priten.makwana7@gmail.com>
2023-04-01 17:08:32 +0000

    revert changes for saves

--- save/models.py
+++ save/models.py
@@ -8,15 +8,14 @@ class Save(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     posts = models.ForeignKey(
         Post, related_name='saved', on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
     galleryposts = models.ForeignKey(
         GalleryPost, related_name='saved', on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ['-created_on']
-        unique_together = ['owner', 'posts']
-        unique_together = ['owner', 'galleryposts']
+        unique_together = ['owner', 'posts', 'galleryposts']
 
     def __str__(self):
-        return f'{self.owner} {self.posts}'
-        return f'{self.owner} {self.galleryposts}'
+        return f'{self.owner} {self.posts}, {self.galleryposts}'
