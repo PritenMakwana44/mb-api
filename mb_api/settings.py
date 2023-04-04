8343fda0df3038b16a4c29922731225dc7a6e361
PritenMakwana44 <priten.makwana7@gmail.com>
2023-04-04 20:35:53 +0000

    Attempt to fix heroku deploy again

--- mb_api/settings.py
+++ mb_api/settings.py
@@ -70,6 +70,20 @@ ALLOWED_HOSTS = [
     'localhost',
 ]
 
+if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGINS = [
+        os.environ.get('CLIENT_ORIGIN')	
+    ]	
+if 'CLIENT_ORIGIN_DEV' in os.environ:	
+    extracted_url = re.match(	
+        r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE	
+    ).group(0)	
+    CORS_ALLOWED_ORIGIN_REGEXES = [	
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",	
+    ]	
+CORS_ALLOW_CREDENTIALS = True
+ROOT_URLCONF = 'mb_api.urls'
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -92,8 +106,6 @@ INSTALLED_APPS = [
     'dj_rest_auth.registration',
     'corsheaders',
     
-
-
     'comments',
     'contact',
     'followers',
@@ -115,30 +127,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ALLOWED_HOSTS = [
-    os.environ.get('ALLOWED_HOST'),
-    'localhost',
-]
-
-if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN')
-    ]
-
-if 'CLIENT_ORIGIN_DEV' in os.environ:
-    extracted_url = re.match(
-        r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE
-    ).group(0)
-    CORS_ALLOWED_ORIGIN_REGEXES = [
-        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
-    ]
-
-
-
-
-CORS_ALLOW_CREDENTIALS = True
-
-ROOT_URLCONF = 'mb_api.urls'
 
 TEMPLATES = [
     {
@@ -162,17 +150,14 @@ WSGI_APPLICATION = 'mb_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DEV' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-    }
+DATABASES = {
+    'default': ({
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',	
+    } if 'DEV' in os.environ else dj_database_url.parse(
+        os.environ.get('DATABASE_URL')	
+    ))	
+}
 
 
 # Password validation
@@ -218,16 +203,4 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'root': {
-        'handlers': ['console'],
-        'level': 'WARNING',
-    },
-}
+
